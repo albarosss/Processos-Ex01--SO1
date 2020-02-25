8e7c5b7c2553d38485846cf7a0a0f632a22ec143
guroc <guroc@DESKTOP-J8EV0C7.lan>
2020-02-25 00:32:28 -0300

    Matar Processos - Ex 2

--- Exercícios Processos - SO1/src/controller/RedesController.java
+++ Exercícios Processos - SO1/src/controller/RedesController.java
@@ -9,7 +9,6 @@ import javax.swing.JOptionPane;
 
 public class RedesController {
 	
-	
 	public void ip(String SO) {
 		String command;
 		String Adap;
@@ -56,16 +55,15 @@ public class RedesController {
 			command="ping -c 10 ";
 		}
 		try {
-			
 			Process p = Runtime.getRuntime().exec(command + dominio);
 			InputStream stream = p.getInputStream();
 			InputStreamReader reader = new InputStreamReader(stream);
 			BufferedReader buffer = new BufferedReader(reader);
 			String line = buffer.readLine();
 			while (line != null) {
-				if(line.contains("time") || line.contains("TLL")) {
+				if(line.contains("time") || line.contains("tempo")) {
 					if(SO.contains("Windows")) {
-						line=line.substring(line.indexOf(" t"), line.lastIndexOf(" "));
+						line=line.substring(line.indexOf("tempo"), line.length());
 					}else {
 						line=line.substring(line.indexOf("tim"), line.length());
 					}
@@ -80,4 +78,80 @@ public class RedesController {
 			System.err.println(e.getMessage());
 		}
 	}
+	
+	public void ListaProcessos (String SO) {
+		String command;
+		if(SO.contains("Windows")) {
+			command = "tasklist";
+		}else {
+			command = "top";
+		}
+		try {
+			Process p = Runtime.getRuntime().exec(command);
+			InputStream stream = p.getInputStream();
+			InputStreamReader reader = new InputStreamReader(stream);
+			BufferedReader buffer = new BufferedReader(reader);
+			String line = buffer.readLine();
+			while (line != null) {
+				System.out.println(line);
+				line = buffer.readLine();
+			}
+			buffer.close();
+			reader.close();
+			stream.close();
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	public void matar (int pid, String SO) {
+		String command;
+		if(SO.contains("Windows")) {
+			command = "TASKKILL /PID";
+		}else {
+			command = "kill";
+		}
+		StringBuffer buffer = new StringBuffer();
+			buffer.append(command);
+			buffer.append(" ");
+			buffer.append(pid);
+			callProcess(buffer.toString());
+	}
+
+
+	public void matar (String nome, String SO) {
+		String command;
+		if(SO.contains("Windows")) {
+			command = "TASKKILL /IM";
+		}else {
+			command = "killall";
+		}
+		StringBuffer buffer = new StringBuffer();
+			buffer.append(command);
+			buffer.append(" ");
+			buffer.append(nome);
+			callProcess(buffer.toString());
+	}
+	
+	public void callProcess(String command) {
+		try {
+			Runtime.getRuntime().exec(command);
+		} catch (IOException e) { 
+			if(e.getMessage().contains("740")) {
+				StringBuffer buffer = new StringBuffer();
+				buffer.append("cmd /c");
+				buffer.append(" ");
+				buffer.append(command);
+				try {
+					Runtime.getRuntime().exec(buffer.toString());
+				} catch (IOException e1) {
+					System.err.println(e1.getMessage());
+				}
+			}else {
+				System.err.println("Commando inválido");
+			}
+		}
+
+	}
+	
 }
--- Exercícios Processos - SO1/src/view/Principal.java
+++ Exercícios Processos - SO1/src/view/Principal.java
@@ -12,16 +12,25 @@ public class Principal {
 		System.out.println(SO);
 		int opc = 0;
 		while(opc != 9) {
-			opc = Integer.parseInt(JOptionPane.showInputDialog("Digite uma opção:\n \n1 - IP \n2 - Ping \n9 - Sair"));
+			opc = Integer.parseInt(JOptionPane.showInputDialog("Digite uma opção:\n \n1 - IP \n2 - Ping \n3 - Mata (Nome ou PID) \n9 - Sair"));
 			switch(opc) {
 				case 1:	CallController.ip(SO);
 					break;
 				case 2:	CallController.ping(SO);
 					break;
+				case 3: CallController.ListaProcessos(SO);
+						String Processo = JOptionPane.showInputDialog("Digite o processo que deseja matar");
+						try {
+							CallController.matar(Integer.parseInt(Processo), SO);
+						}catch(NumberFormatException e){
+							CallController.matar(Processo, SO);
+						}
+					break;
 				case 9:	System.exit(0);
 				default: JOptionPane.showMessageDialog(null, "Valor inválido");
-					break;
 			}
 		}
+		
+		
 	}
 }
